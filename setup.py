3d4c6ba789e4beeef2c34bc8f7b4aa24db7bfe94
Discordmodsbers <94165954+Discordmodsbers@users.noreply.github.com>
2022-11-15 11:44:37 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 #Run this if you want to run 'diablo -h' and not 'python3 diablo.py -h'
 
-USRDIR = '/usr/bin/
+USRDIR = '/usr/bin/'
 USRDIR2 = '/usr/bin/D14BL0'
 import os
 
 os.system(f'git clone https://github.com/Discordmodsbers/D14BL0 {USRDIR}')
-os.system(f'mv diablo.py {USRDIR2}')
-os.system(f"cd {USRDIR2}")
 os.system('chmod +x diablo.py')
+os.system(f'mv diablo.py {USRDIR2}')
+
+with open('bashrc', 'a') as f:
+  f.write("alias diablo='python3 /usr/bin/D14BL0 diablo.py'")
+  f.close()
+ 
+print("Install finished please run 'diablo -h'")
